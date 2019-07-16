dd0c0fbef93d626c418eedbe2c26f0efadcd39d4
Milad Mikeal <miladmikeal@gmail.com>
2019-07-15 20:08:20 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ if (process.emitWarning.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

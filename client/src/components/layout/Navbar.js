7ad7a79fccd2bc63e4564726a8bdf83cf5e4731b
Milad Mikeal <miladmikeal@gmail.com>
2020-03-05 21:28:36 -0800

    Change primary color

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,28 @@
-import React, { Fragment, useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import AuthContext from '../../context/auth/authContext';
-import ContactContext from '../../context/contact/contactContext';
+import React, { Fragment, useContext } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import AuthContext from '../../context/auth/authContext'
+import ContactContext from '../../context/contact/contactContext'
 
 const Navbar = ({ title, icon }) => {
-  const authContext = useContext(AuthContext);
-  const contactContext = useContext(ContactContext);
+  const authContext = useContext(AuthContext)
+  const contactContext = useContext(ContactContext)
 
-  const { isAuthenticated, logout, user } = authContext;
-  const { clearContacts } = contactContext;
+  const { isAuthenticated, logout, user } = authContext
+  const { clearContacts } = contactContext
 
   const onLogout = () => {
-    logout();
-    clearContacts();
+    logout()
+    clearContacts()
   }
 
   const authLinks = (
     <Fragment>
       <li>Hello {user && user.name}</li>
       <li>
-        <a onClick={onLogout} href="#!">
-          <i className="fas fa-sign-out-alt" />{' '}<span className="hide-sm">Logout</span>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
         </a>
       </li>
     </Fragment>
@@ -39,13 +40,11 @@ const Navbar = ({ title, icon }) => {
   )
 
   return (
-    <div className="navbar bg-primary">
+    <div className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
-      <ul>
-        {isAuthenticated ? authLinks : guestLinks}
-      </ul>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   )
 }
@@ -61,4 +60,3 @@ Navbar.defaultProps = {
 }
 
 export default Navbar
-
